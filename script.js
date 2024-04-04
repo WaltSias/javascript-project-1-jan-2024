5bd14c822bef13cae23c02a344faf4fbd24628d6
WaltSias <walt.sias@gmail.com>
2024-04-04 17:28:40 -0500

    added alert

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-// Guess Who?
-let Guesswho = function (hasHair, hasNoglasses, wearingHat) {
-    let hasNOglasses = true
-    let wearingHat = true
-    let hasGlasses = false
-    let hasHair = true
-
-    let guessWho = isMary
-
-    let isMary = hasHair && hasNOglasses && wearingHat;
-    let isHerman = hasHair && hasNoglasses && !wearingHat;
-    let isAnne = hasHair && hasNogalsses && !wearingHat;
-    let letTom = hasHair && hasGlasses && !wearingHat;
-
-    if (isMary) {
-        console.log("Congrats I am Mary");
-        guessWho = "Mary"
-        if (isHerman) {
-            console.log("umm Not quite the answer")
-            guessWho = "Herman"
-        } else if (isTom) {
-            console.log("well I have glass, so no!")
-            guessWho = "Tom"
-        } if (isAnne) {
-            console.log("not me Anne, I dont have a hat")
-            guessWho = "Anne"
-        }
-    } return guesswho;
-
-
-}
-
 getGuess.onclick = function () {
-    let Guess = findGuesswho(true, true, true);
+    let Guess = findguesswho(true, true, true);
     console.log(guess);
     resultDiv.textContent = Guess;
+
+    function guessWho() {
+        alert("You are Correct!");
+    }
+    let guesswho = function (hasHair, hasNoglasses, wearingHat) {
+        let hasNOglasses = true
+        let wearingHat = true
+        let hasGlasses = false
+        let hasHair = true
+
+        let guessWho = isMary
+
+        let isMary = hasHair && hasNOglasses && wearingHat;
+        let isHerman = hasHair && hasNoglasses && !wearingHat;
+        let isAnne = hasHair && hasNoglasses && !wearingHat;
+        let isTom = hasHair && hasGlasses && !wearingHat;
+
+        if (isMary) {
+            alert = ("Congrats I am Mary");
+            guessWho = "Mary"
+            if (isHerman) {
+                alert = ("umm Not quite the answer")
+                guessWho = "Herman"
+            } else if (isTom) {
+                alert = ("well I have glass, so no!")
+                guessWho = "Tom"
+            } if (isAnne) {
+                alert = ("not me Anne, I don't have a hat")
+                guessWho = "Anne"
+            }
+        }
+        return guesswho;
+
+    }
 
 }
